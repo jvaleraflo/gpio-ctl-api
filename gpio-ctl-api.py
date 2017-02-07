a88406ea48fd9c5e4b0e048cdfa2acd82c47a8c5
Juan José Valera Flores <juanjose.valera@gmail.com>
2017-02-07 11:06:43 +0100

    1

--- gpio-ctl-api.py
+++ gpio-ctl-api.py
@@ -1,23 +1,54 @@
+# HTTP Method URI Action
+# ===============================================================================
+# GET         http://0.0.0.0/GPIOs/       Devuelve el estado de todos los gpios
+# GET         http://0.0.0.0/GPIOs/[gpio] Devuelve el estado de un gpio
+# POST        http://0.0.0.0/GPIOs/[gpio] Inicia la gestion de un gpio
+# DELETE      http://0.0.0.0/GPIOs/[gpio] Retira la gestion de un gpio
+
+# PUT         http://0.0.0.0/GPIOs/[gpio]/[value] Modifica las caracteristicas de un gpio
+# ===============================================================================
+
+
 from flask import Flask, request
 from flask_restful import Resource, Api
 
 import RPi.GPIO as GPIO
 
-
 app = Flask(__name__)
 api = Api(app)
 
-todos = {}
+# Establezco modo BCM para referenciar los gpios
+GPIO.setmode(GPIO.BCM)
 
-class TodoSimple(Resource):
+# Diccionario con los gpios almacenando sus caracteristicas
+# key. numero de GPIO
+# values. uso: dir: value
+# 17 : {'uso' : 'relay enchufe 1', 'dir' : 'out', 'value' : 'low'}
+gpios = {}
+
+
+
+class Gpios_List_API(Resource):
     def get(self, todo_id):
-        return {todo_id: todos[todo_id]}
+        return {gpios}
 
-    def put(self, todo_id):
-        todos[todo_id] = request.form['data']
-        return {todo_id: todos[todo_id]}
 
-api.add_resource(TodoSimple, '/<string:todo_id>')
+class Gpio_Do_API(Resource):
+    def get(self, gpio):
+        return {gpios.get(gpio)}
+
+    def POST(self, gpio):
+        gpios.update(gpio)
+        return {'insertado ' + gpio}
+    
+    def delete(self, gpio):
+        gpios.pop(gpio)
+        return {'borrado ' + gpio}
+
+
+api.add_resource(Gpios_List_API, '/GPIOs')
+api.add_resource(Gpio_Do_API,'/GPIOs/<int:gpio>')
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, port=2008, host='0.0.0.0')
